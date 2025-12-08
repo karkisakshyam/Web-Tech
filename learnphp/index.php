60c285c38929f0f6a0f04bc9c9689139421c2636
Sakshyam karki <karkisakshyam5@gmail.com>
2025-12-08 09:03:50 +0545

    learinng php

--- learnphp/index.php
+++ learnphp/index.php
@@ -1,3 +1,18 @@
 <?php
 echo "Hello world";
+echo "<br>";
+
+$name = "Sakshyam karki";
+$age = 19;
+
+echo "Hi my name is $name and i am $age";
+?>
+
+<?php
+$num1 = 10;
+
+$num2 = 20;
+echo "<br>";
+echo "Addition of two numbers";
+echo $num1 + $num2
 ?>
