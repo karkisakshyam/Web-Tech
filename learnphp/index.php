de0478d5b4e4447254c2c5eb39f66f07f1c56517
Sakshyam karki <karkisakshyam5@gmail.com>
2025-12-08 09:14:41 +0545

    learing basics

--- learnphp/index.php
+++ learnphp/index.php
@@ -10,9 +10,19 @@ echo "Hi my name is $name and i am $age";
 
 <?php
 $num1 = 10;
-
 $num2 = 20;
+$result = $num1 + $num2;
 echo "<br>";
-echo "Addition of two numbers";
-echo $num1 + $num2
+echo "Addition of two numbers"." ";
+echo $result;
+echo "<br>";
+
+if($num1 > $num2){
+    echo "num1 is bigger";
+}else{
+    echo "num2 is bigger";
+}
 ?>
+
+
+
