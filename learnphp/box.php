94dec995bd9659e2ae08078925884af244ef7853
Sakshyam karki <karkisakshyam5@gmail.com>
2025-12-09 09:10:19 +0545

    making calender

--- learnphp/box.php
+++ learnphp/box.php
@@ -1,19 +1,31 @@
 <style>
 #box{
-    height:100px;
+   height: 100px;
     width: 100px;
     background-color: red;
-    margin-bottom: 5px;
-    display:inline-block;
-    
+    margin: 2px;
+    display: inline-flex;      
+    justify-content: center;   
+    align-items: center;       
+    font-size: 30px;
+    color: white;
+    font-weight: bold;
+
 }
 </style>
 
 
 <?php
-for ($row = 1; $row <= 4; $row++ ){
-    for($col = 1; $col <= 4; $col++ ){
-       echo "<div id = 'box'> </div>";
+$num = 1;
+for ($row = 1; $row <= 5; $row++ ){
+    for($col = 1; $col <= 7; $col++ ){
+       echo "<div id = 'box'>{$num} </div>";
+       if ($num<=30){
+        $num++;
+       }else{
+        $num=0;
+        $num++;
+       }
     }
     echo "<br>";
 }
